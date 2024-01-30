2b6e984b388fab134a9cfcb6635bf29f4e51940f
Hamdi Taha <hamdi.taha@esprit.tn>
2024-01-30 16:29:40 +0100

    done

---
+++ Seance1/exercice.js
@@ -0,0 +1,15 @@
+
+let students = [{name: 'john',id:  123,marks:98},
+{name: 'john2',id:  444,marks:23},
+{name: 'john3',id:  1243,marks:45},
+{name: 'john4',id:  1233,marks:75},
+]
+
+let total = students.map((student) => {
+    if(student.marks < 50){
+        student.marks += 15
+    }
+    return student.marks
+}
+).filter((mark) => mark > 50).reduce((acc, mark) => acc + mark, 0)
+console.log(total)
